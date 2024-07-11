06dc3075d97a9a4a4c3cec81501ef09dba5a97b6
Ashish Raut <theashish32@gmail.com>
2024-07-11 16:13:13 +0530

    Time: 0 ms (100%), Space: 40.5 MB (16.07%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.java
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.java
@@ -1,15 +1,17 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        int divisibleNum = 1;
-        int ans = 0;
-
-        while(divisibleNum >= 0){
-            if(divisibleNum % n == 0 && divisibleNum % 2 == 0){
-                ans = divisibleNum;
+        int num = n;
+        int ans = -1;
+        for(int i = 2; ; i++){
+            if(num % n == 0 && num % 2 == 0){
+                ans = num;
                 break;
             }
-            divisibleNum++;
+            else{
+                num++;
+            }
         }
         return ans;
+        
     }
 }
